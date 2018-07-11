54d3cedf9270c69eb518fbe68ac4cc297eaf5be6
Alex <mereacre@gmail.com>
2018-07-11 23:02:42 +0100

    npm version

--- test/npm-test.js
+++ test/npm-test.js
@@ -11,7 +11,7 @@ const TDX_SCHEMA = {
         "__tdxType": ["number"],
       },
     },
-    "uniqueIndex": [],
+    "uniqueIndex": [{"asc": "prop1"}],
   },
 };
 
@@ -20,33 +20,18 @@ const testData = [];
 
 sqliteUtils.openDatabase("", "memory", "w+")
     .then((db) => {
-      dbIter = db
+      dbIter = db;
       return sqliteUtils.createDataset(db, TDX_SCHEMA);
     })
     .then(() => {
       return sqliteUtils.createDataset(dbIter, TDX_SCHEMA);
     })
     .then(() => {
-      for (let idx = 0; idx < 100; idx++) {
-        testData.push({
-          prop1: idx,
-          prop2: 100 - idx - 1,
-        });
-      }
+      testData.push({prop1: 0, prop2: 99});
+      testData.push({prop1: 0, prop2: 99});
       return sqliteUtils.addData(dbIter, testData);
     })
-    .then(() => {
-      return sqliteUtils.getDatasetData(dbIter,
-        {$and: [{$or: [{prop1: {$gte: 2, $lte: 5}}, {prop1: {$gte: 92}}]}, {prop2: {$lte: 10}}]},
-        null,
-        {
-          sort: {
-            prop1: 1,
-            prop2: -1,
-          },
-        });
-    })
     .then((result) => {
-      // console.log(result);
+      console.log(result);
     });
   
